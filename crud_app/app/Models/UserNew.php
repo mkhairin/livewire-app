3a866514a000c4d0d376a5ab3026585ef82d70f0
Muhammad Khairin <88011174+mkhairin@users.noreply.github.com>
2025-08-13 12:07:14 +0800

    make users model relation to position

--- crud_app/app/Models/UserNew.php
+++ crud_app/app/Models/UserNew.php
@@ -2,9 +2,34 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Model;
 
-class UserNew extends Model
+class UserNew extends Authenticatable implements HasFactory, Model
 {
-    //
+    use HasFactory;
+
+    protected $table = "user_news";
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'role',
+        'position_id'
+    ];
+
+    protected $hidden = [
+        'password'
+    ];
+
+    protected $casts = [
+        'password' => 'hashed',
+    ];
+
+    public function position()
+    {
+        return $this->belongsTo(Position::class);
+    }
 }
